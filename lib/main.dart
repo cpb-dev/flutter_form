1f3c8be8e6ba2900ba05f13aada0993245549b01
Conor Bailey <cpbailey025@gmail.com>
2023-01-30 15:33:23 +0000

    First alteration

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,9 +21,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'First Flutter Build'),
     );
   }
 }
@@ -99,6 +100,14 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
+            ),
+            const Padding(padding: EdgeInsets.symmetric(horizontal: 8,vertical: 16),
+            child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Type Something",
+            ),
+            ),
             ),
           ],
         ),
