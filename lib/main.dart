af42322fafc9df326bdfc26c634c97945e1957d3
Conor Bailey <cpbailey025@gmail.com>
2023-02-03 17:11:32 +0000

    Retype Password

--- lib/main.dart
+++ lib/main.dart
@@ -128,6 +128,24 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
 
+            Column(
+              children: [
+                TextFormField(
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter Password correctly again!';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: "Retype Password",
+                    border: UnderlineInputBorder(),
+                  ),
+                ),
+              ],
+            ),
+
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 16.0),
               child: ElevatedButton(
