ae6db4e0bf99f90125f3308d7155ad27bb53a728
Conor Bailey <cpbailey025@gmail.com>
2023-02-03 17:09:36 +0000

    Adding Email Input Field

--- lib/main.dart
+++ lib/main.dart
@@ -92,6 +92,24 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget> [
             nameRow,
 
+            Column(
+              children: [
+                TextFormField(
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter an Email!';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    border: UnderlineInputBorder(),
+                  ),
+                ),
+              ],
+            ),
+
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 16.0),
               child: ElevatedButton(
